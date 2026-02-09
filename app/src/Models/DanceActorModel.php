3ba642653373cfcec859c39580cc1d4c2cdf17c1
Shiva Lamichhane <721254@student.inholland.nl>
2026-02-09 15:14:34 +0100

    added random code
    fsdfsdf

--- app/src/Models/DanceActorModel.php
+++ app/src/Models/DanceActorModel.php
@@ -2,3 +2,16 @@
 namespace App\Models;
 use App\Enums\Address;
 use App\Enums\UserRole;
+ 
+    public function saveOrder(int $userId, string $address, string $paymentMethod, int $total) :int
+    {
+        $sql = "INSERT INTO orders (UserId, Address, PaymentMethod, TotalAmount)
+                VALUES (:userId, :address, :paymentMethod, :total)";
+
+        $db = $this->getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->execute([
+            ':userId'           => $userId,
+            ':address'          => $address,
+            ':paymentMethod'    => $paymentMethod,
+            ':total'            => $tot
